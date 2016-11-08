e0067ebf9f1d67507edd91e634cde17e099a5c01
Oh Dakyeong <yds03167@naver.com>
2016-11-08 23:54:46 +0900

    제어-10: 3, 5배수의 합계

---
+++ SumThreeOrFive.java
@@ -0,0 +1,19 @@
+public class SumThreeOrFive {
+	public static void sum(){
+		int sum=0;
+		for(int i=0;i<1000;i++){
+			if(i%3==0){
+				sum+=i;
+			}
+			if(i%5==0){
+				sum+=i;
+			}
+		}
+		System.out.println(sum);
+	}
+	public static void main(String[] args){
+		SumThreeOrFive stof=new SumThreeOrFive();
+		stof.sum();
+	}
+
+}
