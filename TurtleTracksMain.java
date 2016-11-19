ea9c19fecb55131975baa6e09e3208071eb8ae8c
Oh Dakyeong <yds03167@naver.com>
2016-11-19 15:22:04 +0900

    데이터구조-3: 도형을 데이터구조로 저장하고 그리기

---
+++ TurtleTracksMain.java
@@ -0,0 +1,16 @@
+import ch.aplu.turtle.*;
+
+public class TurtleTracksMain{
+	static void drawSquareFrom(double[][] tracks){
+		//t1.pendown();
+		Turtle t1=new Turtle();
+		for(double[] t:tracks){
+			t1.moveTo(t[0],t[1]);
+			System.out.printf("(%.1f,%.1f",t[0],t[1]);
+		}
+	}
+	public static void main(String[] args) {
+		double[][] tracks={{100,100},{200,100},{200,200},{100,200},{100,100}};
+		drawSquareFrom(tracks);
+	}
+}
