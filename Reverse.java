2f60f88274e2f3f5950dd40b61ef7c9e8d8badb3
Oh Dakyeong <yds03167@naver.com>
2016-11-20 19:02:32 +0900

    데이터구조-6: 문자열 'hello' 뒤집기

--- Reverse.java
+++ Reverse.java
@@ -3,6 +3,8 @@ class Reverse{
   static String s="hello";
   static StringBuffer sb= new StringBuffer(s);
   public static void main(String[] args){
+    System.out.print(sb);
+    System.out.print("->");
     sb.reverse();
     System.out.println(sb);
   }
