e33e25b90d27d071d8b59d62f4620c7c89b16792
Oh Dakyeong <yds03167@naver.com>
2016-11-27 23:05:02 +0900

    데이터구조-14: '학생' 객체를 정렬하기

---
+++ ArrayStudentObject.java
@@ -0,0 +1,20 @@
+﻿import java.util.ArrayList;
+import java.util.Collections;
+class ArrayStudentObject {
+  private String name;
+  private int studentNum;
+  ArrayStudentObject(String name, int sNum) {
+    this.name=name;
+    this.studentNum=sNum;
+  }
+  public String getName() {
+  return name;
+  }
+  public static void main(String[] args) {
+    ArrayList<String> studentNameList=new ArrayList<String>();
+    ArrayStudentObject s1=new ArrayStudentObject("odk",201611084);
+    studentNameList.add(s1.getName());
+    Collections.sort(studentNameList);
+    System.out.println(studentNameList);
+  }
+}
