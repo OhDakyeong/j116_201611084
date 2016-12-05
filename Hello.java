217ae913c749d17c9510f4b00b1fdd7f79f6ffe3
Oh Dakyeong <yds03167@naver.com>
2016-12-05 12:38:46 +0900

    문제 oo-5: 객체를 배열로 저장해서, sayHello() (객체의 배열)

--- Hello.java
+++ Hello.java
@@ -1,6 +1,20 @@
-class Hello {
-String toWhom=" world";
-void sayHello() {
-System.out.println("Hello"+toWhom);
-}
+class Hello{
+  String name;
+  Hello(String name){
+    this.name = name;
+  }
+  void sayHello(){
+    System.out.println("Hello " + name);
+  }
+  public static void main(String[] args){
+    Hello[] h = new Hello[5];
+    h[0] = new Hello("dk1");
+    h[1] = new Hello("dk2");
+    h[2] = new Hello("dk3");
+    h[3] = new Hello("dk4");
+    h[4] = new Hello("dk5");
+    for (int i =0; i<h.length; i++){
+      h[i].sayHello();
+    }
+  }
 }
